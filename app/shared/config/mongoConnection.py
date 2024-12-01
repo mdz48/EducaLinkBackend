2cdab61a0e6188410453f512cb4511a59018db61
mdz48 <maximilianodiazclemen.cetis138@gmail.com>
2024-11-30 23:39:27 -0600

    🐛 DB connection

--- app/shared/config/mongoConnection.py
+++ app/shared/config/mongoConnection.py
@@ -1,6 +1,8 @@
 from pymongo.mongo_client import MongoClient
 from sqlalchemy.ext.declarative import declarative_base
-uri = "mongodb+srv://maxdiaz:1234@educalink.n5d8z.mongodb.net/?retryWrites=true&w=majority&appName=EducaLink"
+import os
+
+uri = os.getenv("MONGO_URL")
 
 # Create a new client and connect to the server
 client = MongoClient(uri)
